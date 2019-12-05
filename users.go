f5b10f3c733d456a531422a87e5553b777333344
Alexander Mustafin <sashker@users.noreply.github.com>
2019-12-05 13:31:08 +0200

    Exented User schema

--- users.go
+++ users.go
@@ -10,17 +10,19 @@ type UsersAPIService service
 
 type (
 	User struct {
-		ID        string   `json:"id,omitempty"`
-		Username  string   `json:"username,omitempty"`
-		FirstName string   `json:"first_name,omitempty"`
-		LastName  string   `json:"last_name,omitempty"`
-		Title     string   `json:"title,omitempty"`
-		Email     string   `json:"email,omitempty"`
-		Enabled   bool     `json:"enabled,omitempty"`
-		Features  []string `json:"features,omitempty"`
-		Verified  bool     `json:"verified,omitempty"`
-		PrivLevel string   `json:"priv_level,omitempty"`
-		Timezone  string   `json:"timezone,omitempty"`
+		ID         string   `json:"id,omitempty"`
+		Username   string   `json:"username,omitempty"`
+		Password   string   `json:"password,omitempty"`
+		FirstName  string   `json:"first_name,omitempty"`
+		LastName   string   `json:"last_name,omitempty"`
+		Title      string   `json:"title,omitempty"`
+		Email      string   `json:"email,omitempty"`
+		PresenceID string   `json:"presence_id,omitempty"`
+		Enabled    bool     `json:"enabled,omitempty"`
+		Features   []string `json:"features,omitempty"`
+		Verified   bool     `json:"verified,omitempty"`
+		PrivLevel  string   `json:"priv_level,omitempty"`
+		Timezone   string   `json:"timezone,omitempty"`
 	}
 )
 
