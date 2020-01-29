d481412ded1655cfbb5735c90287d554daad5c15
Alexander Mustafin <sashker@users.noreply.github.com>
2020-01-29 10:26:55 +0200

    Added ChangeAccount request

--- accounts.go
+++ accounts.go
@@ -150,6 +150,70 @@ func (api *AccountsAPIService) CreateAccount(ctx context.Context, input *Account
 	return acc, nil
 }
 
+//ChangeAccount enables to PATCH an existing account document
+func (api *AccountsAPIService) ChangeAccount(ctx context.Context, id string, input map[string]interface{}) (acc *Account, err error) {
+	var (
+		response struct {
+			Data Account `json:"data"`
+			ResponseEnvelope
+		}
+	)
+
+	if id == "" || len(id) != 32 {
+		return nil, reportError("specify correct account id")
+	}
+
+	/*if input.Realm == "" {
+		return nil, reportError("realm is required field")
+	}*/
+
+	params := Request{
+		CTX:    ctx,
+		Method: "PATCH",
+		Path:   api.client.cfg.BasePath + "/accounts/" + id,
+	}
+
+	reqBody := RequestEnvelope{
+		Data: input,
+	}
+
+	jsonString, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, reportError("can't marshall body for request")
+	}
+
+	body, err := setBody(jsonString, "json")
+	if err != nil {
+		return nil, reportError("can't prepare body for the request")
+	}
+
+	params.PostBody = body
+
+	req, err := api.client.prepareRequest(&params)
+	if err != nil {
+		return nil, reportError("Can't prepare a request %s", err)
+	}
+
+	resp, err := api.client.callAPI(ctx, req)
+	if err != nil || resp == nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		return nil, prepareError(resp)
+	}
+
+	err = readBody(resp, &response)
+	if err != nil {
+		return nil, reportError("Can't decode response: %v", err)
+	}
+
+	acc = &response.Data
+
+	return acc, nil
+}
+
 func (api *AccountsAPIService) ListChildren(ctx context.Context, acc string, disablePagination bool) (chldrn []Child, err error) {
 	var response struct {
 		Data []Child `json:"data"`
